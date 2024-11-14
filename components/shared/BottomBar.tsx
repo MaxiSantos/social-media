34c00dd6ba143c7d5b01c505d7bf4da7afd72206
maxi-code <max_011_1@hotmail.com>
2024-11-14 15:52:25 -0300

    feat: :bug: using clerkProvider with dynamic ofr nextjs error

--- components/shared/BottomBar.tsx
+++ components/shared/BottomBar.tsx
@@ -1,52 +1,60 @@
 'use client'
-import React from 'react'
+import React, { Suspense } from 'react'
 import { sidebarLinks } from '@/constants'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link';
 import Image from 'next/image';
-import { useAuth } from '@clerk/nextjs';
+import { ClerkProvider, useAuth } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server'
+import Skeleton from 'react-loading-skeleton';
+
 
 type Props = {}
 
 const BottomBar = () => {
   const pathname = usePathname()
   const { userId } = useAuth()
+
   return (
-    <>
-      <section className="bottombar">
-        <div className="bottombar_container">
-          {
-            sidebarLinks.map((link) => {
-              const isActive = (pathname.includes(link.route) && link.route.length > 1 || pathname === link.route)
-              if (link.route === '/profile') {
-                link.route = `${link.route}/${userId}`
+  <>
+    <section className="bottombar">
+      <div className="bottombar_container">
+        <Suspense fallback={<Skeleton />}>
+          <ClerkProvider dynamic>
+            {
+              sidebarLinks.map((link) => {
+                const isActive = (pathname.includes(link.route) && link.route.length > 1 || pathname === link.route)
+                if (link.route === '/profile') {
+                  link.route = `${link.route}/${userId}`
+                }
+                return (
+                  <Link
+                    href={link.route}
+                    key={link.label}
+                    className={` bottombar_link ${isActive &&
+                      'bg-primary-500'
+                      }`}
+                  >
+                    <Image
+                      src={link.imgURL}
+                      alt={link.label}
+                      width={24}
+                      height={24}
+                    />
+                    <p className='text-light-1 text-subtle-medium'>
+                      {link.label}
+                    </p>
+                  </Link>
+                )
               }
-              return (
-                <Link
-                  href={link.route}
-                  key={link.label}
-                  className={` bottombar_link ${isActive &&
-                    'bg-primary-500'
-                    }`}
-                >
-                  <Image
-                    src={link.imgURL}
-                    alt={link.label}
-                    width={24}
-                    height={24}
-                  />
-                  <p className='text-light-1 text-subtle-medium'>
-                    {link.label}
-                  </p>
-                </Link>
               )
             }
-            )
-          }
-        </div>
-      </section>
-    </>
-  )
+          </ClerkProvider>
+        </Suspense>
+      </div>
+    </section>
+  </>
+)
 }
 
 export default BottomBar
--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -1,9 +1,11 @@
 'use client'
 import { sidebarLinks } from '@/constants'
 import { usePathname } from 'next/navigation'
-import { useAuth } from '@clerk/nextjs';
+import { ClerkProvider, useAuth } from '@clerk/nextjs';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Suspense } from 'react';
+import Skeleton from 'react-loading-skeleton';
 
 const LeftSideBar = () => {
   const pathname = usePathname()
@@ -12,34 +14,38 @@ const LeftSideBar = () => {
     <>
       <section className="leftsidebar custom-scrollbar">
         <div className="flex w-full flex-1 flex-col gap-6 px-6">
-          {
-            sidebarLinks.map((link) => {
-              const isActive = (pathname.includes(link.route) && link.route.length > 1 || pathname === link.route)
-              if (link.route === '/profile') {
-                link.route = `${link.route}/${userId}`
+          <Suspense fallback={<Skeleton />}>
+            <ClerkProvider dynamic>
+              {
+                sidebarLinks.map((link) => {
+                  const isActive = (pathname.includes(link.route) && link.route.length > 1 || pathname === link.route)
+                  if (link.route === '/profile') {
+                    link.route = `${link.route}/${userId}`
+                  }
+                  return (
+                    <Link
+                      href={link.route}
+                      key={link.label}
+                      className={` leftsidebar_link ${isActive &&
+                        'bg-primary-500'
+                        }`}
+                    >
+                      <Image
+                        src={link.imgURL}
+                        alt={link.label}
+                        width={24}
+                        height={24}
+                      />
+                      <p className='text-light-1'>
+                        {link.label}
+                      </p>
+                    </Link>
+                  )
+                }
+                )
               }
-              return (
-                <Link
-                  href={link.route}
-                  key={link.label}
-                  className={` leftsidebar_link ${isActive &&
-                    'bg-primary-500'
-                    }`}
-                >
-                  <Image
-                    src={link.imgURL}
-                    alt={link.label}
-                    width={24}
-                    height={24}
-                  />
-                  <p className='text-light-1'>
-                    {link.label}
-                  </p>
-                </Link>
-              )
-            }
-            )
-          }
+            </ClerkProvider>
+          </Suspense>
         </div>
       </section>
     </>
