3a570a5087471f4d304872c522f0436df01589c3
maxi-code <max_011_1@hotmail.com>
2024-11-14 09:15:11 -0300

    feat: :sparkles: adding top bar and bottom bar

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import { OrganizationSwitcher, SignedIn, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { dark } from '@clerk/themes'
 
 type Props = {}
 
-const TopBar = (props: Props) => {
+const TopBar = () => {
   return (
-    <div>BottomBar</div>
+    <>
+      <nav className="topbar">
+        <Link
+          href='/'
+          className="flex items-center gap-4"
+        >
+          <Image
+            src='/assets/logo.svg'
+            alt="logo"
+            width={75}
+            height={75}
+          />
+        </Link>
+        <div className="flex items-center gap-3">
+          <SignedIn>
+            <OrganizationSwitcher
+              appearance={
+                {
+                  baseTheme: dark,
+                  elements: {
+                    organizationSwitcherTrigger: 'py-2 px-4'
+                  }
+                }
+              }
+            />
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
+    </>
   )
 }
 
