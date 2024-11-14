a4004c625a89ad6c1a0417cf6892c0b8173a2383
maxi-code <max_011_1@hotmail.com>
2024-11-14 16:48:54 -0300

    build: :green_heart: updaed eslint so automatic build on vercel works so we can test clerk webhook

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,26 +3,29 @@ import User from "../models/user.model"
 import { connectToDB } from "../db";
 
 interface CreateUserParams {
-    userId: string;
-    email?: string;
-    username: string;
-    name?: string;
-    image?: string
+  userId: string;
+  email: string;
+  username: string;
+  name: string;
+  image: string
 }
 export const createUser = async ({
-    userId,
-    email,
-    name,
-    username,
-    image
+  userId,
+  email,
+  name,
+  username,
+  image
 }: CreateUserParams): Promise<void> => {
-    try{
-        connectToDB()
-        await User.create({
-            id: userId,
-            username: username?.toLowerCase()
-        })
-    } catch(err: any) {
-        throw new Error(`Failed to create user: ${err.message}`)
-    }
+  try {
+    await connectToDB()
+    await User.create({
+      id: userId,
+      username: username?.toLowerCase(),
+      email,
+      name,
+      image
+    })
+  } catch (err: any) {
+    throw new Error(`Failed to create user: ${err.message}`)
+  }
 }
