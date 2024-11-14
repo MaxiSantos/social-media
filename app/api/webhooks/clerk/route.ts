0103b644f9a07e7ac8e350b9b4d1d25b72bebd93
maxi-code <max_011_1@hotmail.com>
2024-11-14 16:58:29 -0300

    build: :green_heart: test commit to trigger deploy on vercel

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -6,7 +6,7 @@ import { createUser } from '@/lib/actions/user.actions'
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
-  console.log("THIS WEBHOOK IS WORKING")
+  console.log("webhook POST request")
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
