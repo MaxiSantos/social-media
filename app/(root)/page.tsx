a3b3bccb61ac81d0fae19db1000403524af43ca0
maxi-code <max_011_1@hotmail.com>
2024-11-18 21:16:22 -0300

    feat: :sparkles: updating home page to get tweet and also upgrading get searchParams to nextjs15

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,23 @@
+import { fetchTweets } from "@/lib/actions/tweet.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import LandingPage from "components/sections/LandingPage";
 
-export default async function Home() {
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>
+ 
+export default async function Home(props: {  
+  searchParams: SearchParams
+}) {
+  const searchParams = await props.searchParams
   const user = await currentUser();
   
   if(!user){
     return <LandingPage />
   }
+
+  const result = await fetchTweets( 
+    searchParams.page ? +searchParams.page : 1,
+    3
+  );
   return (
     <main>
       <h1 className="text-center">Social media</h1>
