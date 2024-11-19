3bd98e63fe11f326fad494ef70c6dd791a84d836
maxi-code <max_011_1@hotmail.com>
2024-11-18 22:14:13 -0300

    feat: :sparkles: deleting button and action

--- lib/actions/tweet.actions.ts
+++ lib/actions/tweet.actions.ts
@@ -113,7 +113,7 @@ export async function likeOrDislikeTweet(userId: string, tweetId: string, path: 
 
       // Remove the tweet from the user's likedTweets array
       user.likedTweets = user.likedTweets.filter((id: any) => id.toString() !== tweetId);
-    
+
     } else {
       // If the tweet is not liked, increment its likes and add it to the user's likedTweets
       tweet = await Tweet.findByIdAndUpdate(
@@ -121,7 +121,7 @@ export async function likeOrDislikeTweet(userId: string, tweetId: string, path: 
         { $inc: { likes: 1 } },
         { new: true } // Return the updated document
       );
-      
+
       // Add the tweet to the user's likedTweets array
       user.likedTweets.push(tweetId);
     }
@@ -137,6 +137,114 @@ export async function likeOrDislikeTweet(userId: string, tweetId: string, path: 
   }
 }
 
+export const isTweetByUser = async (userId: string, tweetId: string) => {
+  try {
+    connectToDB();
+    const tweet = await Tweet.findById(tweetId);
+    if (!tweet) throw new Error('Tweet not found');
+
+    return tweet?.author.toString() === userId?.toString();
+  } catch (error: any) {
+    throw new Error(`Failed to check tweet ownership: ${error.message}`);
+  }
+}
+export const deleteTweet = async (userId: string, tweetId: string, path: string) => {
+  try {
+    connectToDB();
+    // Check if the tweet exists and if the user is the author
+    const tweet = await Tweet.findById(tweetId);
+    if (!tweet) throw new Error('Tweet not found');
+
+    // Check if the tweet belongs to the user
+    const isAuthor = await isTweetByUser(userId, tweetId);
+    if (!isAuthor) throw new Error('You are not authorized to delete this tweet');
+
+    // Remove tweet reference from the user's tweets array
+    await User.findByIdAndUpdate(userId, {
+      $pull: { tweets: tweetId }
+    });
+
+    // If the tweet is a retweet, remove it from the user's retweets array
+    if (tweet.retweetOf) {
+      await User.findByIdAndUpdate(userId, {
+        $pull: { retweets: tweet.retweetOf }
+      });
+    }
+
+    // tweet.parentId !== null => its a comment
+    // check if this tweet is a comment
+    if (tweet.parentId) {
+      await User.findByIdAndUpdate(userId, {
+        $pull: { replies: tweetId }
+      });
+      await Tweet.findByIdAndUpdate(tweet.parentId, {
+        $pull: { children: tweetId }
+      });
+    }
+
+    // Remove tweet reference from the group if it belongs to a group
+    if (tweet.group) {
+      await Group.findByIdAndUpdate(tweet.group, {
+        $pull: { tweets: tweetId }
+      });
+    }
+
+    // Remove tweet reference from any group collections it might belong to
+    // This dosent make sense
+    /*await Group.updateMany(
+      { tweets: tweetId },
+      { $pull: { tweets: tweetId } }
+    );*/
+
+    // Find and delete all retweets of the tweet
+    const retweets = await Tweet.find({ retweetOf: tweetId });
+
+    for (const retweet of retweets) {
+      // For now when creating a retweet this goes to tweets and retweets user model
+      // Remove retweet reference from the user's tweets array
+      await User.findByIdAndUpdate(retweet.author, {
+        $pull: { tweets: retweet._id }
+      });
+
+      // Remove retweet reference from the user's retweets array
+      await User.findByIdAndUpdate(retweet.author, {
+        $pull: { retweets: retweet._id }
+      });
+
+      // Remove retweet reference from the group if it belongs to a group
+      /*if (retweet.group) {
+        await Group.findByIdAndUpdate(retweet.group, {
+          $pull: { tweets: retweet._id }
+        });
+      }*/
+
+      // Remove retweet reference from any group collections it might belong to
+      await Group.updateMany(
+        { tweets: retweet._id },
+        { $pull: { tweets: retweet._id } }
+      );
+
+      //Delete the retweet
+      await Tweet.findByIdAndDelete(retweet._id);
+    }
+
+    // Remove the tweet from all users' retweets arrays
+    await User.updateMany(
+      { retweets: tweetId },
+      { $pull: { retweets: tweetId } }
+    );
+
+    // Delete the original tweet
+    await Tweet.findByIdAndDelete(tweetId);
+
+    revalidatePath(path);
+
+  } catch (error: any) {
+    throw new Error(`Failed to delete tweet: ${error.message}`);
+  }
+
+};
+
 interface RetweetParams {
   userId: string;
   tweetId: string;
@@ -144,7 +252,7 @@ interface RetweetParams {
   groupId: string | null;
 }
 export const retweetTweet = async ({
-  userId, 
+  userId,
   tweetId,
   path,
   groupId,
@@ -152,7 +260,7 @@ export const retweetTweet = async ({
   try {
     connectToDB();
     const user = await User.findById(userId);
-    if (!user) throw new Error('User not found:');    
+    if (!user) throw new Error('User not found:');
 
     const originalTweet = await Tweet.findById(tweetId);
     if (!originalTweet) throw new Error('Original tweet not found');
